550944fa2e370beed22e5796b32f327eda3a84de
Saeed Altout <saeedaltoutpro@gmail.com>
2025-07-31 16:03:14 +0300

    Update dependencies in package.json and package-lock.json, remove unused App.css, and refactor App component to implement routing with React Router. Introduce ThemeProvider and Toaster for enhanced theming and notifications. Add loading state to Button component.

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
+import { Toaster } from "sonner";
+import { ThemeProvider } from "./components/theme-provider";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      <App />
+      <Toaster />
+    </ThemeProvider>
+  </StrictMode>
+);
---
+++ src/modules/auth/index.ts
@@ -0,0 +1,5 @@
+export { default as AuthLayout } from "./layout";
+export * from "./pages";
+export * from "./components";
+export * from "./schemas";
+export * from "./api";
